529b92a448cb09e8c8ce74143e8744b22ebf8bee
Adiansyah Putra <adiansyahhputra@gmail.com>
2023-03-24 13:43:25 +0700

    feat: login, read and create categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PageSignin from './pages/signin';
+import SigninPage from './pages/signin';
+import DashboardPage from './pages/dashboard';
+import CategoriesPage from './pages/categories';
+import CategoriesCreate from './pages/categories/create';
+import CategoriesEdit from './pages/categories/edit';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>home</>} />
-        <Route path="/signin" element={<PageSignin />} />
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/categories/create" element={<CategoriesCreate />} />
+        <Route path="/categories/edit/:id" element={<CategoriesEdit />} />
       </Routes>
     </BrowserRouter>
   );
